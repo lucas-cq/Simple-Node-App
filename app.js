c17896312885c3b2c8ada651d39a2a3dee12ed06
Lucas Cummings <lucas.cummings024@gmail.com>
2021-11-01 16:53:48 -0600

    Brute force, works but needs error handling

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
-  if (argv.beer = 'IPA' && argv.amount < 2) {
+  if (argv.beer === 'IPA' && argv.amount <= 2) {
+
     console.log('Thats it?')
-  } else if (argv.beer = 'IPA' && argv.amount > 3){
+
+  } else if (argv.beer === 'IPA' && argv.amount > 2) {
+
     console.log('I need a nap')
-  } else {
-    console.log('Please give me a beer')
-  }
 
-  if (argv.beer = 'Lite' && argv.amount < 2) {
+  } else if (argv.beer === 'Lite' && argv.amount <= 2) {
+
     console.log('Is this water?')
-  } else if (argv.beer = 'Lite' && argv.amount > 3) {
-    console.log('Wheres the urinals?!')
-  } else {
-    console.log('Please give me a beer')
-  }
 
-  if (argv.beer = 'Stout' && argv.amount < 2) {
+  } else if (argv.beer === 'Lite' && argv.amount > 2) {
+
+    console.log('Wheres the urinals?!')
+
+  } else if (argv.beer === 'Stout' && argv.amount <= 2) {
+
     console.log('Yuck!')
-  } else if (argv.beer = 'Stout' && argv.amount > 3) {
+
+  } else if (argv.beer === 'Stout' && argv.amount > 2) {
+
     console.log('Maybe these arent so bad');
+
   } else {
-    console.log('Please give me a beer')
+
+    console.log('Please give me a beer and an amount')
+
   }
