d952c3b05dc1e4380bf8f61bd04686b16d565b97
Lucas Cummings <lucas.cummings024@gmail.com>
2021-11-02 16:11:05 -0600

    Added license, readme, error handling

---
+++ README.md
@@ -0,0 +1,17 @@
+# CPNT262-A4 - Lucas Cummings
+
+## Give the Terminal a Beer!
+Give the terminal a type of beer and an amount.
+
+### Commands
+```
+--beer=IPA --amount=1
+```
+
+```
+--beer=Lite --amount=2
+```
+
+```
+--beer=Stout --amount=3
+```
--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+// Code to run Yargs
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 
-  if (argv.beer === 'IPA' && argv.amount <= 2) {
+// An "if else" statement for each type of beer and amount
 
+  if (!argv.beer) {
+    //If a type of beer is not listed, this will run
+    console.log('Please enter a type of beer')
+
+  } else if (!argv.amount) {
+    //If an amount of beer is not listed, this will run
+    console.log('Please enter an amount of beer')
+
+  } else if (argv.beer === 'IPA' && argv.amount <= 2) {
+    // This returns a line regarding too little beer if the amount is less or equal to 2
     console.log('Thats it?')
 
   } else if (argv.beer === 'IPA' && argv.amount > 2) {
-
+    // This returns a line regarding too much beer if the amount is greater than 2
     console.log('I need a nap')
 
   } else if (argv.beer === 'Lite' && argv.amount <= 2) {
 
     console.log('Is this water?')
 
-  } else if (argv.beer === 'Lite' && argv.amount > 2) {
+  } else if (argv.beer === 'Lite' && argv.amount > 2) { 
 
     console.log('Wheres the urinals?!')
 
@@ -27,7 +38,6 @@ const argv = yargs(hideBin(process.argv)).argv
     console.log('Maybe these arent so bad');
 
   } else {
-
+    //If all else fails this will display a broad error
     console.log('Please give me a beer and an amount')
-
   }
